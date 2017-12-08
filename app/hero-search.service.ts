b4c19ab6c706d9efa3734ba504077adfc7d2ba91
iconojdiazch <khepherer@gmail.com>
2017-12-08 12:01:27 +0100

    Search service corregido

--- app/hero-search.service.ts
+++ app/hero-search.service.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Headers, Http, Response } from '@angular/http';
 import { Observable } from 'rxjs';
 
 import { Hero } from './hero';
 
+import { AuthenticationService } from './authentication.service';
+
 @Injectable()
 export class HeroSearchService {
+  private headers = new Headers({
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + this.authenticationService.getToken()
+  });
 
-  constructor(private http: Http) {}
+  constructor(private http: Http, private authenticationService: AuthenticationService) { }
 
   search(term: string): Observable<Hero[]> {
     return this.http
-      .get(`http://localhost:8080/heroes/search/findByName?name=${term}`)
+      .get(`http://localhost:8080/heroes/search/findByName?name=${term}`, { headers: this.headers })
       .map((r: Response) => r.json()._embedded.heroes as Hero[]);
-    }
+  }
 }
